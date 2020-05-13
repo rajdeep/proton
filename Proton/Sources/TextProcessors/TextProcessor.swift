90d72321642444ea385dc17648a92671ef9a9462
Rob Chatfield <rchatfield@atlassian.com>
2020-05-13 19:01:32 +1000

    pre-calculate sorted processors

--- Proton/Sources/TextProcessors/TextProcessor.swift
+++ Proton/Sources/TextProcessors/TextProcessor.swift
@@ -22,16 +22,18 @@ import Foundation
 import UIKit
 
 class TextProcessor: NSObject, NSTextStorageDelegate {
-    private(set) var activeProcessors = [TextProcessing]()
+    private(set) var activeProcessors = [TextProcessing]() {
+        didSet {
+            sortedProcessors = activeProcessors.sorted { $0.priority > $1.priority }
+        }
+    }
+    private(set) var sortedProcessors = [TextProcessing]()
     weak var editor: EditorView?
 
     init(editor: EditorView) {
         self.editor = editor
     }
 
-    var sortedProcessors: [TextProcessing] {
-        return activeProcessors.sorted { $0.priority > $1.priority }
-    }
 
     func register(_ processor: TextProcessing) {
         register([processor])
