e5f4845df59d788e8bcd014218b19b2eefe4a911
Michal Smialko <michal.smialko@gmail.com>
2021-08-05 14:54:27 +0200

    Fix: NSTextView's delegate was not set.

--- Proton/Sources/Swift/PlatformBridges/macOS/TextView.swift
+++ Proton/Sources/Swift/PlatformBridges/macOS/TextView.swift
@@ -95,6 +95,15 @@ public class PlatformTextView: NSTextView, NSTextViewDelegate {
         set { enclosingScrollView?.contentInsets = newValue }
     }
     
+    public override init(frame frameRect: NSRect, textContainer container: NSTextContainer?) {
+        super.init(frame: frameRect, textContainer: container)
+        delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - UIView Bridge
     
     public override func layout() {
