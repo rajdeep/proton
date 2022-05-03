0ab70776b7cc3eb396d0cf002aff2e6d96c75450
Rajdeep Kwatra <rajkwatra@gmail.com>
2022-05-03 19:34:40 +1000

    Updated snapshots for iPhone 13 iOS 15.2

--- Proton/Tests/Editor/EditorSnapshotTests.swift
+++ Proton/Tests/Editor/EditorSnapshotTests.swift
@@ -30,7 +30,7 @@ class EditorSnapshotTests: XCTestCase {
     override func setUp() {
         super.setUp()
 
-        recordMode = true
+        recordMode = false
     }
 
     func testRendersPlaceholder() {
