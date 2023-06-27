68b71456ebbaccfb8e3d3a97aa526505321e69a7
Rajdeep Kwatra <rajkwatra@gmail.com>
2023-06-27 10:24:15 +1000

    Updated asyncTextResolver to validate if original string range has changed

--- Proton/Tests/AsyncTextResolver/AsyncTextResolverSnapshotTests.swift
+++ Proton/Tests/AsyncTextResolver/AsyncTextResolverSnapshotTests.swift
@@ -27,46 +27,110 @@ import SnapshotTesting
 class AsyncTextResolverSnapshotTests: SnapshotTestCase {
     func testIgnoresTextAsyncAttributeUntilInvoked() {
         let expectation = functionExpectation()
-        expectation.expectedFulfillmentCount = 2
-        let viewController = EditorTestViewController(height: 80)
+        let viewController = EditorTestViewController(height: 130)
         let editor = viewController.editor
         editor.asyncTextResolvers = [DummyResolver()]
-        editor.attributedText = NSAttributedString(string: "This is some text")
-        editor.addAttribute(.asyncTextResolver, value: "dummy", at: NSRange(location: 8, length: 4))
+        let text = NSMutableAttributedString(string: "This is some text with link ")
+        text.append(NSAttributedString(string: "http://google.com", attributes: [.asyncTextResolver: "dummy"]))
+        text.append(NSAttributedString(string: " and another link "))
+        text.append(NSAttributedString(string: "http://abc.com", attributes: [.asyncTextResolver: "dummy"]))
+        text.append(NSAttributedString(string: " followed by some more text"))
+        editor.setNeedsAsyncTextResolution()
+        editor.attributedText = text
 
-        viewController.render(size: CGSize(width: 300, height: 150))
+        viewController.render(size: CGSize(width: 300, height: 170))
         assertSnapshot(matching: viewController.view, as: .image, record: recordMode)
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-            viewController.render(size: CGSize(width: 300, height: 150))
-            assertSnapshot(matching: viewController.view, as: Snapshotting.image, record: self.recordMode)
-            expectation.fulfill()
-            // Invoke async text resolution
-            editor.resolveAsyncTextIfNeeded()
-        }
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            viewController.render(size: CGSize(width: 300, height: 150))
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+            viewController.render(size: CGSize(width: 300, height: 170))
             assertSnapshot(matching: viewController.view, as: Snapshotting.image, record: self.recordMode)
             expectation.fulfill()
         }
 
         waitForExpectations(timeout: 1.0)
     }
+
+    func testResolvesAsyncAttributeInOrder() {
+        let expectation = functionExpectation()
+        let viewController = EditorTestViewController(height: 130)
+        let editor = viewController.editor
+        editor.asyncTextResolvers = [DummyResolver()]
+        let text = NSMutableAttributedString(string: "This is some text with link ")
+        text.append(NSAttributedString(string: "http://abc.com", attributes: [.asyncTextResolver: "dummy"]))
+        text.append(NSAttributedString(string: " and another link "))
+        text.append(NSAttributedString(string: "http://google.com", attributes: [.asyncTextResolver: "dummy"]))
+        text.append(NSAttributedString(string: " followed by some more text"))
+        editor.setNeedsAsyncTextResolution()
+        editor.attributedText = text
+
+        viewController.render(size: CGSize(width: 300, height: 170))
+        assertSnapshot(matching: viewController.view, as: .image, record: recordMode)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+            viewController.render(size: CGSize(width: 300, height: 170))
+            assertSnapshot(matching: viewController.view, as: Snapshotting.image, record: self.recordMode)
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 1.0)
+    }
+
+    func testDiscardsAsyncAttributeInOrder() {
+        let expectation = functionExpectation()
+        let viewController = EditorTestViewController(height: 130)
+        let editor = viewController.editor
+        editor.asyncTextResolvers = [DummyResolver()]
+        let text = NSMutableAttributedString(string: "This is some text with link ")
+        text.append(NSAttributedString(string: "http://abc.com", attributes: [.asyncTextResolver: "dummy"]))
+        text.append(NSAttributedString(string: " and another link "))
+        text.append(NSAttributedString(string: "http://google.com", attributes: [.asyncTextResolver: "dummy"]))
+        text.append(NSAttributedString(string: " followed by some more text"))
+        editor.setNeedsAsyncTextResolution()
+        editor.attributedText = text
+        // break the link
+        editor.replaceCharacters(in: NSRange(location: 65, length: 0), with: NSAttributedString(string: " "))
+
+        viewController.render(size: CGSize(width: 300, height: 170))
+        assertSnapshot(matching: viewController.view, as: .image, record: recordMode)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+            viewController.render(size: CGSize(width: 300, height: 170))
+            assertSnapshot(matching: viewController.view, as: Snapshotting.image, record: self.recordMode)
+
+            let attributeRange = viewController.editor.attributedText.rangeOf(attribute: .asyncTextResolver, startingLocation: 0)
+            XCTAssertNil(attributeRange)
+
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 1.0)
+    }
+
+
 }
 
 class DummyResolver: AsyncTextResolving {
     var name: String { "dummy" }
 
     func resolve(using editor: EditorView, range: NSRange, string: NSAttributedString, completion: @escaping (AsyncTextResolvingResult) -> Void) {
-        let mutableString = NSMutableAttributedString(attributedString: string)
-
-        mutableString.addAttribute(.foregroundColor, value: UIColor.red, range: mutableString.fullRange)
-        mutableString.removeAttribute(.asyncTextResolver, range: mutableString.fullRange)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-            if editor.attributedText.attributedSubstring(from: range).string == string.string {
+        let mutableString: NSMutableAttributedString
+        if string.string == "http://google.com" {
+            mutableString = NSMutableAttributedString(string: "Google")
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                mutableString.addAttribute(.foregroundColor, value: UIColor.red, range: mutableString.fullRange)
+                mutableString.removeAttribute(.asyncTextResolver, range: mutableString.fullRange)
                 completion(.apply(mutableString, range: range))
-            } else {
+            }
+        } else if string.string == "http://abc.com" {
+            mutableString = NSMutableAttributedString(string: "Australian Broadcasting Corporation")
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                mutableString.addAttribute(.foregroundColor, value: UIColor.blue, range: mutableString.fullRange)
+                mutableString.removeAttribute(.asyncTextResolver, range: mutableString.fullRange)
+                completion(.apply(mutableString, range: range))
+            }
+        } else {
+            mutableString = NSMutableAttributedString(attributedString: string)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                 completion(.discard)
             }
         }
