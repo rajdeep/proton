1e867ddd46ed9b8d95c119b813c1fbe0d8d2b9b7
Rajdeep Kwatra <rajkwatra@gmail.com>
2024-03-22 14:22:13 +1100

    Fixed begin and end editing calls in text storage editing

--- Proton/Sources/ObjC/PRTextStorage.m
+++ Proton/Sources/ObjC/PRTextStorage.m
@@ -23,7 +23,6 @@
 
 @interface PRTextStorage ()
 @property (nonatomic) NSTextStorage *storage;
-@property (nonatomic, assign) BOOL isEditingAttributedText;
 @end
 
 @implementation PRTextStorage
@@ -75,10 +74,6 @@
 }
 
 - (void)replaceCharactersInRange:(NSRange)range withAttributedString:(NSAttributedString *)attrString {
-    _isEditingAttributedText = YES;
-    [self beginEditing];
-    // TODO: Add undo behaviour
-    
     // Handles the crash when nested list receives enter key in quick succession that unindents the list item.
     // Check only required with Obj-C based TextStorage
     if ((range.location + range.length) > _storage.length) {
@@ -106,31 +101,24 @@
 
     NSAttributedString *deletedText = [_storage attributedSubstringFromRange:range];
     [_textStorageDelegate textStorage:self will:deletedText insertText:replacementString in:range];
-    NSArray<NSTextAttachment *> *attachmentsToDelete = [self attachmentsForRange:range];
-    [super replaceCharactersInRange:range withAttributedString:replacementString];
-
-    [self endEditing];
-    _isEditingAttributedText = NO;
-    // Deleting of Attachment needs to happen after editing has ended. If invoked while textStorage editing is
-    // taking place, this may sometimes result in a crash(_fillLayoutHoleForCharacterRange).
-    [self deleteAttachments:attachmentsToDelete];
+    [super replaceCharactersInRange:range withAttributedString: replacementString];
 }
 
 - (void)replaceCharactersInRange:(NSRange)range withString:(NSString *)str {
+    // Capture any attachments in the original range to be deleted after editing is complete
     NSArray<NSTextAttachment *> *attachmentsToDelete = [self attachmentsForRange:range];
-    if(_isEditingAttributedText == NO) {
-        [self beginEditing];
-    }
+
+    [self beginEditing];
+
     NSInteger delta = str.length - range.length;
     [_storage replaceCharactersInRange:range withString:str];
     [_storage fixAttributesInRange:NSMakeRange(0, _storage.length)];
     [self edited:NSTextStorageEditedCharacters & NSTextStorageEditedAttributes range:range changeInLength:delta];
-    if(_isEditingAttributedText == NO) {
-        [self endEditing];
-        // Deleting of Attachment needs to happen after editing has ended. If invoked while textStorage editing is
-        // taking place, this may sometimes result in a crash(_fillLayoutHoleForCharacterRange).
-        [self deleteAttachments:attachmentsToDelete];
-    }
+
+    [self endEditing];
+    // Deleting of Attachment needs to happen after editing has ended. If invoked while textStorage editing is
+    // taking place, this may sometimes result in a crash(_fillLayoutHoleForCharacterRange).
+    [self deleteAttachments:attachmentsToDelete];
 }
 
 -(void)deleteAttachments:(NSArray<NSTextAttachment *>*) attachments {
