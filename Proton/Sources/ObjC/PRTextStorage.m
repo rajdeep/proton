ff969952a23112b46d09aacd01937e2ddf000be6
Rajdeep Kwatra <rajkwatra@gmail.com>
2024-04-24 15:23:39 +1000

    updated comment

--- Proton/Sources/ObjC/PRTextStorage.m
+++ Proton/Sources/ObjC/PRTextStorage.m
@@ -118,8 +118,10 @@
 - (void)replaceCharactersInRange:(NSRange)range withString:(NSString *)str {
     // Capture any attachments in the original range to be deleted after editing is complete
     NSArray<NSTextAttachment *> *attachmentsToDelete = [self attachmentsForRange:range];
-    // Deleting of Attachment needs to happen after editing has ended. If invoked while textStorage editing is
+    // Deleting of Attachment needs to happen outside editing flow. If invoked while textStorage editing is
     // taking place, this may sometimes result in a crash(_fillLayoutHoleForCharacterRange).
+    // If invoked after, it may still cause a crash as caret location is queried which may cause editor layout again
+    // resulting in the crash.
     [self deleteAttachments:attachmentsToDelete];
     [self beginEditing];
 
